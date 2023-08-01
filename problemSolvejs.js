ff9c292ba1d17414f920f1373af93b64d5ea9cdc
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-08-01 08:43:19 +0300

    init:commit

--- problemSolvejs.js
+++ problemSolvejs.js
@@ -1,3 +1,27 @@
+function countCharacters(character) {
+  // Helper function to count the occurrence of each character in a character
+  const charCount = new Map();
+  for (const char of character.toLowerCase()) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+  return charCount;
+}
+
+function areAnagrams(character1, character2) {
+  // Helper function to check if two characters are anagrams
+  if (character1.length !== character2.length) {
+    return false;
+  }
+  const charCount1 = countCharacters(character1);
+  const charCount2 = countCharacters(character2);
+  for (const [char, count] of charCount1.entries()) {
+    if (charCount2.get(char) !== count) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function isAnagramInArray(anagram, arr) {
   const sortedAnagram = anagram.toLowerCase().split("").sort().join("");
   const arrayOfAnagrams = [];
